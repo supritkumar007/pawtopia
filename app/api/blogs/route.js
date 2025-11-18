e4acedb64caff928e57b8a6a0ea24f8837eb94b3
supritkumar007 <supritkumar9353@gmail.com>
2025-11-18 14:21:58 +0530

    Add debugging logs to blogs API to troubleshoot 500 error

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -5,11 +5,19 @@ import Blog from '@/lib/models/Blog';
 // GET /api/blogs - Get all published blogs (Public)
 export async function GET() {
   try {
+    console.log('Connecting to database for blogs...');
     await connectDB();
+    console.log('Database connected successfully for blogs');
 
+    console.log('Fetching blogs...');
     const blogs = await Blog.find({ published: true })
       .populate('author', 'name')
       .sort({ createdAt: -1 });
+
+    console.log('Found blogs:', blogs.length);
+    if (blogs.length > 0) {
+      console.log('First blog:', { id: blogs[0]._id, title: blogs[0].title, author: blogs[0].author });
+    }
 
     return NextResponse.json({
       success: true,
@@ -17,6 +25,7 @@ export async function GET() {
       data: blogs
     });
   } catch (error) {
+    console.error('Error fetching blogs:', error);
     return NextResponse.json({
       success: false,
       message: error.message
